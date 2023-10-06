a693293cfb185ca921baf04db0be7dbc3fc2062b
Claudio Goncalves <claudiojmg1@hotmail.de>
2023-10-06 12:24:27 +0200

    code

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,3 +1,16 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+  return apiData.filter((obj) => {
+    return mandatoryKeys.every((key) => obj.hasOwnProperty(key));
+  });
 }
+const data1 = [{ id: 1, price: 100 }, { price: 50 }];
+const filteredData1 = filterApiData(data1, ["id"]);
+console.log(filteredData1);
+
+const data2 = [
+  { id: 1, price: 100 },
+  { id: 2, title: "" },
+  { id: 3, price: 50 },
+];
+const filteredData2 = filterApiData(data2, ["id", "price"]);
+console.log(filteredData2);
